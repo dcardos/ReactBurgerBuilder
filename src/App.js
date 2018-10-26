933c3c8975bfe4439a4a1b469e6e39f0d792960d
dcardos <cardoso.nilo@gmail.com>
2018-10-26 10:49:09 -0300

    first burger on the screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        eiita
-      </div>
-    );
-  }
+    render() {
+        return (
+        <div>
+            <Layout>
+                <BurgerBuilder></BurgerBuilder>
+            </Layout>
+        </div>
+        );
+    }
 }
 
 export default App;
