aac09f870b331261a78c0ecd37fc161142b527af
dcardos <cardoso.nilo@gmail.com>
2018-10-28 12:43:41 -0300

    modal now animated

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
             meat: 0,
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchsaseState (ingredients) {
@@ -65,6 +66,10 @@ class BurgerBuilder extends Component {
         this.setState( {totalPrice: newPrice, ingredients: updatedIngredients} );
         this.updatePurchsaseState(updatedIngredients);
     }
+
+    purchaseHandler = () => {
+        this.setState( {purchasing: true } );
+    };
     
     render () {
         const disabledInfo = {
@@ -75,7 +80,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -84,6 +89,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice}
                 />
             </>
